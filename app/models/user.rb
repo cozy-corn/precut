1daa545cb003667302a2da98d28c95ca7630eaff
cozy-corn <koji.nakano777@gmail.com>
2025-09-25 20:16:21 +0900

    ユーザーログインログアウトの動作確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+          # フルネーム
+          validates :full_name, presence: true
+          validates :full_name, length: { in: 2..50 }
+          # email
+          validates :email, presence: true
+          # パスワード
+          validates :encrypted_password, presence: true
 end
