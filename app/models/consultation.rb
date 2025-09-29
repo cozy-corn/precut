89e805bdfe39b7cafa91c0e1652a290d2a43d134
cozy-corn <koji.nakano777@gmail.com>
2025-09-29 17:42:02 +0900

    consultationsテーブルの作成

---
+++ app/models/consultation.rb
@@ -0,0 +1,6 @@
+class Consultation < ApplicationRecord
+  belongs_to :user
+
+  enum status: { draft: "draft", completed: "completed", shared: "shared", archived: "archived" }
+  validates :summary, presence: true
+end
