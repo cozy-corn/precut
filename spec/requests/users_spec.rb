9a126406c046e7d9d7e0780b6cc7c9951387a70d
cozy-corn <koji.nakano777@gmail.com>
2025-12-10 18:11:38 +0900

    pullrequestでの修正

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "User Profile", type: :request do
-  let(:user) { create(:user) } 
+  let(:user) { create(:user) }
 
   describe "GET /user (マイページ)" do
     context "ログインしている場合" do
       it "マイページにアクセスし、成功すること" do
-        sign_in user 
-        
-        get user_path 
-        
+        sign_in user
+
+        get user_path
+
         expect(response).to have_http_status(200)
       end
     end
